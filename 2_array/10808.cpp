a90f4969e67d8155d237ea1711097bd171c0a3b2
TaeYong <71390051+art-is-tae@users.noreply.github.com>
2022-08-23 23:14:04 +0900

    Update 10808.cpp

--- 2_array/10808.cpp
+++ 2_array/10808.cpp
@@ -6,8 +6,9 @@ int main(void)
   int narr[26] = {};
   string s;
   cin >> s;
-  for (auto c : s)
+  for (auto c : s) {
     narr[c - 'a']++;
+  }
   for (int i = 0; i < 26; i++)
     cout << narr[i] << ' ';
 }
